898c674d8ea11126bcde601efd1945861f4cec64
Gus Shaal <ghiathchaal.beg@gmail.com>
2023-12-30 21:11:11 +0000

    Added K6 Perfomance testing | Fix threshold

--- MyJourneyToWorkPerfTests/spikeTest.js
+++ MyJourneyToWorkPerfTests/spikeTest.js
@@ -8,10 +8,6 @@ export const options = {
     { duration: "2m", target: 2000 },
     { duration: "1m", target: 0 },
   ],
-  thresholds: {
-    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<500"], // 95 percent of response times must be below 500ms
-  },
 };
 
 export default function () {
--- MyJourneyToWorkPerfTests/stressTest.js
+++ MyJourneyToWorkPerfTests/stressTest.js
@@ -11,10 +11,6 @@ export let options = {
     { duration: "1m", target: 200 },
     { duration: "1m", target: 0 },
   ],
-  thresholds: {
-    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<500"], // 95 percent of response times must be below 500ms
-  },
 };
 
 export default function () {
